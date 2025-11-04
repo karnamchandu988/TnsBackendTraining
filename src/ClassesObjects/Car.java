2050289dcb75adb605cfc9421fd592a4219977f5
hp <hp@DESKTOP-72N58C4>
2025-11-04 18:05:43 +0530

    Types of Car as an example for the Classes and Objects

---
+++ src/ClassesObjects/Car.java
@@ -0,0 +1,9 @@
+package ClassesObjects;
+
+public class Car {
+String name;
+int count;
+void print() {
+	System.out.println("Name"+" "+name+" "+count);
+}
+}
---
+++ src/ClassesObjects/CarMain.java
@@ -0,0 +1,16 @@
+package ClassesObjects;
+public class CarMain {
+
+	public static void main(String[] args) {
+		Car c=new Car();
+		c.name = "Chandu";
+		c.count=1;
+		c.print();
+		
+		Car a=new Car();
+		a.name="Advi";
+		a.count=0;
+		a.print();
+	}
+
+}
