04c40bd01f147c58f4c439a9c70e3a5a1d805e98
hp <hp@DESKTOP-72N58C4>
2025-11-04 17:46:06 +0530

    Practice of Classes and Object

---
+++ src/ClassesObjects/StatesInIndia.java
@@ -0,0 +1,19 @@
+// Practice of the Class and Objects 
+
+// States, Capitals and Districts
+package ClassesObjects;
+
+public class StatesInIndia {
+String name;
+String capital;
+int districts;
+
+
+void print() {
+	System.out.println("State Name: "+name);
+	System.out.println("State Capital: "+capital);
+	System.out.println("No of Distrits: "+districts);
+}
+	
+
+}
