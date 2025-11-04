6d0a1f75346bac262eb03b4ad4da485c292cb1cb
hp <hp@DESKTOP-72N58C4>
2025-11-04 16:52:24 +0530

    Added Classes and Objects practice

---
+++ src/StateMain.java
@@ -0,0 +1,29 @@
+// Main class of the States, Capitals and Districts
+
+public class StateMain {
+
+	public static void main(String[] args) {
+		
+		StatesInIndia s=new StatesInIndia();
+		s.name = "Andhra Pradesh";
+		s.capital = "Amaravathi";
+		s.districts = 29; 
+		s.print();
+		System.out.println();
+		StatesInIndia s1=new StatesInIndia();
+		s1.name = "Telangana";
+		s1.capital = "Hyderabad";
+		s1.districts = 31; 
+		s1.print();
+		System.out.println();
+		
+		StatesInIndia s2=new StatesInIndia();
+		s2.name = "Maharastra";
+		s2.capital = "Mumbai";
+		s2.districts = 43; 
+		s2.print();
+		
+			
+	}
+
+}
